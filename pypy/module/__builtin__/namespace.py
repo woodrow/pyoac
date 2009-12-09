df614e89617e3942d1333072cd9eecbd06f8fb19
Stephen Woodrow <srwoodrow@gmail.com>
2009-12-09 07:06:18 -0500

    Minor changes to namespaces

--- pypy/module/__builtin__/namespace.py
+++ pypy/module/__builtin__/namespace.py
@@ -19,9 +19,11 @@ def newtoken(space):
     from pypy.objspace.std.nametokenobject import W_NametokenObject
     return W_NametokenObject()
     
-def changetoken(space,w_obj,w_token):
-    if id(w_obj) in space.namespace_table:
+    #TODO note that when you create a new token, you'll need to add it to its parents __alltokens__ and changetoken(newtoken,newtoken) to make it self-owned
+    
+def changetoken(space,w_obj,w_token,w_force):
+    if id(w_obj) in space.namespace_table or space.is_true(w_force):
         space.namespace_table[id(w_obj)] = w_token
-        return True
+        return space.w_True
     else:
-        return False
+        return space.w_False
